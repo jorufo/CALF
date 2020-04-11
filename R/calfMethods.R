daaa4f999e461f08381ffd08179121bec6b75f3b
Dayne <dayne.filer@gmail.com>
2020-04-11 19:15:44 -0400

    Add predict method for calf objects

--- R/calfMethods.R
+++ R/calfMethods.R
@@ -8,3 +8,12 @@ print.calf <- function(x, ...) {
                    check.names = FALSE)
   cat("\n Final score (", x$score, "): ", x$finalS, "\n", sep = "")
 }
+
+#'@method predict calf
+#'@export
+
+predict.calf <- function(object, newx) {
+  bvec <- rep(0, ncol(newx))
+  bvec[object$xInd] <- object$weight
+  as.matrix(x) %*% bvec
+}
